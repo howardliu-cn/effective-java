c77c5938022e1aa45c89021b8f87351689a866fb
Howard Liu <howardliu1988@163.com>
2022-01-04 14:52:04 +0800

    java-11-features: NestHost、NestmateOf、NestMember

---
+++ java-features/java-11-features/src/main/java/cn/howardliu/tutorials/java11/nest/NestMain.java
@@ -0,0 +1,19 @@
+package cn.howardliu.tutorials.java11.nest;
+
+import cn.howardliu.tutorials.java11.nest.Outer.Inner;
+
+/**
+ * @author 看山 <a href="mailto:howardliu1988@163.com">Howard Liu</a>
+ * Created on 2021-12-30
+ */
+public class NestMain {
+    public static void main(String[] args) {
+        final String outerNestHostName = Outer.class.getNestHost().getName();
+        System.out.println(outerNestHostName);
+
+        final String innerNestHostName = Inner.class.getNestHost().getName();
+        System.out.println(innerNestHostName);
+
+
+    }
+}
--- java-features/java-11-features/src/test/java/cn/howardliu/tutorials/java11/nest/OuterTest.java
+++ java-features/java-11-features/src/test/java/cn/howardliu/tutorials/java11/nest/OuterTest.java
@@ -1,8 +1,16 @@
 package cn.howardliu.tutorials.java11.nest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+
+import cn.howardliu.tutorials.java11.nest.Outer.Inner;
 
 /**
  * @author 看山 <a href="mailto:howardliu1988@163.com">Howard Liu</a>
@@ -15,5 +23,41 @@ class OuterTest {
 
         assertDoesNotThrow(outer::callInnerReflectionMethod);
         assertDoesNotThrow(outer::callInnerMethod);
+    }
+
+    @Test
+    void checkNestHostName() {
+        final String outerNestHostName = Outer.class.getNestHost().getName();
+        assertEquals("cn.howardliu.tutorials.java11.nest.Outer", outerNestHostName);
+
+        final String innerNestHostName = Inner.class.getNestHost().getName();
+        assertEquals("cn.howardliu.tutorials.java11.nest.Outer", innerNestHostName);
+
+        assertEquals(outerNestHostName, innerNestHostName);
+    }
+
+    @Test
+    void checkIsNestmateOf() {
+        assertTrue(Inner.class.isNestmateOf(Outer.class));
+        assertTrue(Outer.class.isNestmateOf(Inner.class));
+    }
+
+    @Test
+    void getNestMembers() {
+        final List<String> outerNestMembers = Arrays.stream(Outer.class.getNestMembers())
+                .map(Class::getName)
+                .collect(Collectors.toList());
+
+        assertEquals(2, outerNestMembers.size());
+        assertTrue(outerNestMembers.contains("cn.howardliu.tutorials.java11.nest.Outer"));
+        assertTrue(outerNestMembers.contains("cn.howardliu.tutorials.java11.nest.Outer$Inner"));
+
+        final List<String> innerNestMembers = Arrays.stream(Inner.class.getNestMembers())
+                .map(Class::getName)
+                .collect(Collectors.toList());
+
+        assertEquals(2, innerNestMembers.size());
+        assertTrue(innerNestMembers.contains("cn.howardliu.tutorials.java11.nest.Outer"));
+        assertTrue(innerNestMembers.contains("cn.howardliu.tutorials.java11.nest.Outer$Inner"));
     }
 }
